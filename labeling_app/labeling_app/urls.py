0ed91401589310107253af078481ba300ea067f3
Harold Budd <haroldbudd12@gmail.com>
2023-06-14 02:41:02 +0900

    Some changes

--- labeling_app/labeling_app/urls.py
+++ labeling_app/labeling_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # Other URL patterns
+    path('submit_annotation', views.submit_annotation, name='submit_annotation'),
 ]
